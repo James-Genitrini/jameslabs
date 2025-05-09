26ac766c823f2c74c9fd7406a2e897ebc075e92b
James Genitrini <jamesgenitrini@gmail.com>
2025-05-09 13:01:02 +0200

    small screen disable comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <article class="bg-cream p-6 rounded-lg shadow-md max-w-7xl mx-auto mt-8 flex flex-col md:flex-row gap-8" style="height: 75vh;">
-
+        
         <!-- Section Article -->
-        <div class="flex-1 w-full md:w-3/4 h-full overflow-auto flex flex-col">
+        <div class="flex-1 w-full md:w-3/4 h-full overflow-auto">
             <header class="flex items-start mb-8">
                 @if($post->hasMedia('thumbnail'))
                     <div class="w-1/3 bg-gray-200 overflow-hidden rounded-lg">
@@ -21,25 +21,26 @@
                 </div>
             </header>
 
-            <section class="mb-8 flex-1">
+            <section class="mb-8">
                 <h2 class="text-2xl font-semibold text-gray-300 mb-4">Contenu de l'article</h2>
                 <div class="prose max-w-none">
                     {!! Str::markdown($post->content) !!}
                 </div>
             </section>
 
-            <footer class="text-sm text-gray-400 mt-auto">
+            <footer class="text-sm text-gray-400 mt-8">
                 <div class="mt-2">
                     <span class="font-semibold">Crédit : </span>{{ $post->created_at->format('d/m/Y H:i') }} par J.G.
                 </div>
             </footer>
         </div>
 
-        <div class="w-full md:w-1/4 bg-neutral-800 text-gray-200 p-6 rounded-lg max-h-screen flex flex-col">
+        <!-- Section Commentaires (visible sur grands écrans uniquement) -->
+        <div class="w-full md:w-1/4 bg-neutral-800 text-gray-200 p-6 rounded-lg max-h-screen md:block hidden">
             <!-- Like -->
             <x-like-component :post="$post" />
 
-            <div class="space-y-3 mb-6 overflow-y-auto" style="height: 35vh;">
+            <div class="space-y-3 mb-6 overflow-y-auto" style="height: 40vh;">
                 <x-comment-list :post="$post" />
             </div>
 
